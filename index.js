b0757537b2f08d4a43cbc93b84f30ce4303e5aa1
Sarthak Agarwal <agarwal.sarthak001@gmail.com>
2023-08-13 10:35:41 +0530

    feat: add index.js, initial start of the application

---
+++ index.js
@@ -0,0 +1,64 @@
+// index.js
+// where your node app starts
+
+// init project
+require("dotenv").config();
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/whoami", function (req, res) {
+  let ipAddress;
+  let language;
+  let software;
+
+  if (!!req && !!req["headers"]) {
+    const requestHeader = req["headers"];
+    const forwardedForHeaderName = requestHeader["x-forwarded-for"];
+    const realIpHeaderName = requestHeader["x-real-ip"];
+    const headerAcceptLanguage = requestHeader["accept-language"];
+    const headerUserAgent = requestHeader["user-agent"];
+
+    language = headerAcceptLanguage ? headerAcceptLanguage : "";
+    software = headerUserAgent ? headerUserAgent : "";
+
+    let splittedIps = forwardedForHeaderName
+      ? forwardedForHeaderName.split(",")
+      : [];
+
+    ipAddress = splittedIps.length
+      ? splittedIps[0]
+      : realIpHeaderName
+      ? realIpHeaderName
+      : req.connection.remoteAddress;
+  }
+
+  console.log("ipAddress is: ", ipAddress);
+  console.log("remoteAddress ipAddress is: ", req.connection.remoteAddress);
+  console.log("language is: ", language);
+  console.log("software is: ", software);
+
+  res.json({ ipAddress, language, software });
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
